005888bbcf78fa97920d54269cfe50489dbc8e09
ruflanalizada <alizaderuflan@gmail.com>
2026-05-25 18:37:55 -0700

    iframe approach

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+import { useEffect } from "react";
 export default function Home() {
-  return null;
+  useEffect(() => {
+    window.history.replaceState(null, "", "/");
+  }, []);
+  return (
+    <iframe 
+      src="/prod1_05_25_.html" 
+      style={{width:"100%", height:"100vh", border:"none"}}
+    />
+  );
 }
