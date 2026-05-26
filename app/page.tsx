09d2010d15fdde69eb3200969e945ed871f1cc31
ruflanalizada <alizaderuflan@gmail.com>
2026-05-25 18:30:26 -0700

    fix clean url

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-"use client";
-import { useEffect } from "react";
 export default function Home() {
-  useEffect(() => {
-    window.location.href = "/prod1_05_25_.html";
-  }, []);
   return null;
 }
